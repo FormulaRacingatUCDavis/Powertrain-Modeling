6ee98e88da47be772bb269b4a09d04ffb64f3b72
maxboyk <65986640+maxboyk@users.noreply.github.com>
2021-01-08 16:07:11 -0800

    Added PT Config Loop (huh?) and dumped data on to graph
    PT Config Loop : Creates structure containing each Motor / MC / Cell combination, calculates temperature change of cells for each configuration
    -Compatability Checks need revisiting
    -Should we define the structure fields / size before the loop?
    
    Data Dump
    -Default line specs
    -Legend not added

--- Powertrain COTS Analysis/PowertrainCOTSAnalysis.m
+++ Powertrain COTS Analysis/PowertrainCOTSAnalysis.m
@@ -7,57 +7,94 @@ clc; clear; close all;
 %% COTS Database Imports
 [Cell, Controller, Motor] = SpreadsheetImport();
 
-%% Desired Accumulator Characteristics
-% Generate accumulator performance requirements based on the power demand
-% and 
-Pack(1,1,1).Capacity = 1;
+%% Useful Constants
 
-Pack = struct();
-for i = 1 : length(Cell)
+Pack.V = (1:600);    % Pack Voltage [V]
+Pack.E = 6.7;    % Pack Energy Capacity [kWh]
+Endurance = 22156741.88; % Constant from endurance, sum of Current^2 times each time step
+CellCp = 0.902;  % Lithium ion cell specific heat capacity [J/g-K]
+
+%% Powertrain Configuration Sweeping
+% Determine feasible voltage range for each Motor / Controller
+% combination, then sweep these voltage ranges for every Cell, in order to
+% find the Change in Temperature vs. Voltage for every Motor / Controller /
+% Cell combination
+
+Powertrain = struct();
+
+for i = 1 : length(Motor)
     for j = 1 : length(Controller)
-        for k = 1 : length(Motor)
+        for k = 1 : length(Cell)
             
-            Pack(i,j,k).Capacity = Pack(1,1,1).Capacity;
+            Powertrain(i,j,k).Motor = Motor(i);
+            Powertrain(i,j,k).Controller = Controller(j);
+            Powertrain(i,j,k).Cell = Cell(k);
             
-            %%% Compatibility Checks (Motor / Controller Compat)
-            if Controller.Voltage.Max < Motor.Voltage.Min || Controller.Voltage.Min > Motor.Voltage.Max
-               Pack(i,j,k).Flag = 'Controller & Motor Voltages Not Compatible';
-               return
+            Powertrain(i,j,k).Flag = [];
+            
+            Powertrain(i,j,k).VRange = [];      % Defining these as empty in case compatability check
+            Powertrain(i,j,k).Series = [];      % Breaks everything... this fix doesn't actually work
+            Powertrain(i,j,k).Parallel = [];    %
+            Powertrain(i,j,k).Temp = [];        % These should be allocated before the loop somehow
+            
+            %%% Compatibility Checks
+            
+            % (Motor / Controller Compat)
+            if Controller(j).Voltage < Motor(i).Voltage
+               Powertrain(i,j,k).Flag = 'Controller & Motor Voltages Not Compatible | ';
+%                break \\\ commented out to continue compatability checks...
+%                suppose there's no reason to do this, which rasies the
+%                question do we need the debug message or can "Flag" just be
+%                a logical value
             end
             
-            Pack.Power.Cont = 1;
-            Pack.Power.Max  = 1;
+            % (Cell Resistance Has Been Given)
+            if isnan(Cell(k).Resistance)
+                Powertrain(i,j,k).Flag = [Powertrain(i,j,k).Flag, 'No Cell Resistance Value | '];
+%                 break \\\ THIS HAD TO BE COMMENTED OUT BECAUSE IT FUCKS
+%                 EVERYTHING UP
+            end
             
+            %%% Cell Temperature Change
             
-            Pack.Energy
+            % Find Usable Voltage Range For Each Config
+            Powertrain(i,j,k).VRange = [max((min(Powertrain(i,j,k).Motor.Voltage,Powertrain(i,j,k).Controller.Voltage)-200),0) :...
+                                        min(Powertrain(i,j,k).Motor.Voltage,Powertrain(i,j,k).Controller.Voltage)];
+            
+            % Number of Cells in Series / Parallel
+            Powertrain(i,j,k).Series = floor(Powertrain(i,j,k).VRange ./ Cell(k).VoltageMax);
+            Powertrain(i,j,k).Parallel = floor(Pack.E ./ (Powertrain(i,j,k).Series .* Cell(k).VoltageMax .* Cell(k).Capacity) .* 1000);
+            
+            % Temperature Change in Cells , Using Cell Ohmic Heat Gen and
+            % Cell Thermall Mass
+            Powertrain(i,j,k).Temp = (10/6 .* 117.6./Powertrain(i,j,k).VRange).^2 .* (Cell(k).Resistance .*...
+                                      Powertrain(i,j,k).Series ./ Powertrain(i,j,k).Parallel) .* Endurance ./...
+                                      (Cell(k).Mass .* Powertrain(i,j,k).Series .* Powertrain(i,j,k).Parallel .*...
+                                      CellCp);
         end
     end
 end
 
-Pack.V = (1:600);    % Pack Voltage [V]
-Pack.E = 6.7;    % Pack Energy Capacity [kWh]
-
-
-
-%% Pack Calculations
-Accum.S = floor(Pack.V ./ Cell.V);   % Cells in series required to reach desired Voltage
-Accum.P = round(Pack.E ./ (Accum.S .* Cell.V .* Cell.Cap) .* 1000); % Cells in parallel required to reach desired Capacity
-
-Accum.IR = Cell.IR .* Accum.S ./ Accum.P; % Total pack Internal Resistance [ohms]
-Accum.C = Cell.Cp .* Cell.m .* Accum.S .* Accum.P;   % Total cell heat capacity [J/K]
-
-
-%% Filtering Stuff
-
-
-
 %% Plotting Stuff
 figure(1)
-for i = 1:length(Cell.Name)
-plot(Pack.V,Tdelt(i,:))
-hold on
+for i = 1 : length(Motor)
+    for j = 1 : length(Controller)
+        for k = 1 : length(Cell)
+            
+            if isempty(Powertrain(i,j,k).Flag)
+                plot(Powertrain(i,j,k).VRange,Powertrain(i,j,k).Temp);
+                hold on
+            end
+            
+        end
+    end
 end
-hold off
+
+title('A Clever Title')
+xlim([100,600]);
+xlabel('Powertrain Voltage [V]')
+ylim([0,100]);
+ylabel('Endurance Temperature Change [C]')
 
 %% Local Functions   
 function [Cell, Controller, Motor] = SpreadsheetImport()
@@ -75,38 +112,54 @@ function [Cell, Controller, Motor] = SpreadsheetImport()
     Cell = struct();
     Cell( size(Spreadsheet.Cell, 1) - 7 ).Model = [];
     for i = 8 : size(Spreadsheet.Cell, 1)
-       Cell(i-7).Model        = Spreadsheet.Cell(i,1);
-       Cell(i-7).Manufacturer = Spreadsheet.Cell(i,2);
-       Cell(i-7).Chemistry    = Spreadsheet.Cell(i,3);
-       Cell(i-7).Geometry     = Spreadsheet.Cell(i,4);
-       
-       Cell(i-7).Voltage.Nom  = str2double(Spreadsheet.Cell{i,5 });
-       Cell(i-7).Voltage.Max  = str2double(Spreadsheet.Cell{i,6 });
-       
-       Cell(i-7).Capacity     = str2double(Spreadsheet.Cell{i,7 });
-       
-       Cell(i-7).Current.Cont = str2double(Spreadsheet.Cell{i,8 });
-       Cell(i-7).Current.Max  = str2double(Spreadsheet.Cell{i,9 });
-       
-       Cell(i-7).Resistance   = str2double(Spreadsheet.Cell{i,10});
-       
-       Cell(i-7).Mass         = str2double(Spreadsheet.Cell{i,21});
-       
-       Cell(i-7).Cost         = str2double(Spreadsheet.Cell{i,27});
-    end
+        Cell(i-7).Model        = Spreadsheet.Cell(i,1);
+        Cell(i-7).Manufacturer = Spreadsheet.Cell(i,2);
+        Cell(i-7).Chemistry    = Spreadsheet.Cell(i,3);
+        Cell(i-7).Geometry     = Spreadsheet.Cell(i,4);
+
+        Cell(i-7).VoltageNom  = str2double(Spreadsheet.Cell{i,5 });
+        Cell(i-7).VoltageMax  = str2double(Spreadsheet.Cell{i,6 });
+
+        Cell(i-7).Capacity     = str2double(Spreadsheet.Cell{i,7 });
+
+        Cell(i-7).Current.Cont = str2double(Spreadsheet.Cell{i,8 });
+        Cell(i-7).Current.Max  = str2double(Spreadsheet.Cell{i,9 });
+
+        Cell(i-7).Resistance   = str2double(Spreadsheet.Cell{i,10}) ./ 1000; % Internal resistance [mOhms -> Ohms]
+
+        Cell(i-7).Mass         = str2double(Spreadsheet.Cell{i,21});
+
+        Cell(i-7).Cost         = str2double(Spreadsheet.Cell{i,27});
+        end
     
     % Allocate Controller Structure
     Controller = struct();
-    Controller( size(Spreadsheet.Controller, 1) - 7 ).Model = [];
-    for i = 8 : size(Spreadsheet.Controller, 1)
-      
+    Controller( size(Spreadsheet.Controller, 1) - 3 ).Model = [];
+    for i = 4 : size(Spreadsheet.Controller, 1)
+        Controller(i-3).Model = Spreadsheet.Controller(i,1);
+        Controller(i-3).Manufacturer = Spreadsheet.Controller(i,2);
+
+        Controller(i-3).Voltage = str2double(Spreadsheet.Controller{i,5 });
+
+        Controller(i-3).Current.Cont = str2double(Spreadsheet.Controller{i,7 });
+        Controller(i-3).Current.Max = str2double(Spreadsheet.Controller{i,8 });
+
+        Controller(i-3).Mass = str2double(Spreadsheet.Controller{i,20 });
     end
     
     % Allocate Controller Structure
     Motor = struct();
-    Motor( size(Spreadsheet.Motor, 1) - 7 ).Model = [];
-    for i = 8 : size(Spreadsheet.Motor, 1)
-      
+    Motor( size(Spreadsheet.Motor, 1) - 3 ).Model = [];
+    for i = 4 : size(Spreadsheet.Motor, 1)
+        Motor(i-3).Model = Spreadsheet.Motor(i,1);
+        Motor(i-3).Manufacturer = Spreadsheet.Motor(i,2);
+        
+        Motor(i-3).Voltage = str2double(Spreadsheet.Motor{i,5 });
+        
+        Motor(i-3).Current.Cont = str2double(Spreadsheet.Motor{i,7 });
+        Motor(i-3).Current.Max = str2double(Spreadsheet.Motor{i,8 });
+        
+        Motor(i-3).Mass = str2double(Spreadsheet.Motor{i,24 });
     end
     
     %%% Local Spreadsheet Import Function
